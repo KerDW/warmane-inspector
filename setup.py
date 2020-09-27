5f4d568beba2c0c3c48492849443b3ecfeafe7b7
Álvaro Zamora <alvarozajim@gmail.com>
2020-09-27 22:24:45 +0200

    cx_freeze executable setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from cx_Freeze import setup, Executable
+
+exe = [Executable("warmane_scanner.py", base = "Win32GUI")]
+  
+setup(name = "Macaw" , 
+      version = "0.1" , 
+      description = "Scrapes the warmane armory to gather meaningful data about a character." , 
+      executables = exe
+) 

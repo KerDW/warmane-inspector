34484bc334410964248a913e0f2e9818d4f7d8d2
Álvaro Zamora <alvarozajim@gmail.com>
2020-10-02 05:21:45 +0200

    inspector icon

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ exe = [Executable("warmane_scanner.py", base = "Win32GUI")]
 setup(name = "Macaw" , 
       version = "0.1" , 
       description = "Scrapes the warmane armory to gather meaningful data about a character." , 
-      executables = exe
+      executables = exe,
+      icon="icon_inspector.ico"
 ) 
